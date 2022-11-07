48e0a07d1b3bffd229ac71275e94f50c91503e77
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-11-07 21:38:08 +0600

    header done

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Footer will be designed</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar className="H-main" collapseOnSelect expand="lg" bg="" variant="">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto ">
+              <Link className="style-H " href="#features">
+                My Review
+              </Link>
+              <Link className="style-H" href="#pricing">
+                Add Service
+              </Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
