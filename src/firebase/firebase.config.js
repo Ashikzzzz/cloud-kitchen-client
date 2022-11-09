c9eb9c10d4f5c12fdf3d6a059ca326fdfc0c7e8d
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-11-10 04:45:50 +0600

    firebase environment veriable done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOPvTezOanmE3xsNapXo8xXdi4CEKZ9GQ",
-  authDomain: "cloud-kitchen-4989f.firebaseapp.com",
-  projectId: "cloud-kitchen-4989f",
-  storageBucket: "cloud-kitchen-4989f.appspot.com",
-  messagingSenderId: "61573385412",
-  appId: "1:61573385412:web:f865e68d3f844ae7c44d91",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
